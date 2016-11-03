abca76ed0f451f82f3e7075ac38d166e26af673b
Youssef Eldakar <youssefeldakar@gmail.com>
2016-11-03 13:03:35 +0200

    fsize loses initial file position
    According to rewind(3), the function is equivalent to a seek to position
    0.  This is apparently not the intention.  The function has been
    modified to save the initial file position first and finally restore it
    before returning.
    
    P.S. The fsize function is not invoked anywhere in the code so far.  It
    is perhaps not the best practice to rely on file size to make decisions,
    as the file may be growing in certain situations as it is being
    processed.  It may be reasonable to drop this function.

--- libgzmulti/utils.c
+++ libgzmulti/utils.c
@@ -6,14 +6,16 @@ fcopy (FILE *ifile, FILE *ofile, size_t count)
   void *buffer = malloc (count);
   fread (buffer, 1, count, ifile);
   fwrite (buffer, 1, count, ofile);
+
   free (buffer);
 }
 
 size_t
-fsize (FILE * f)
+fsize (FILE *f)
 {
+  int pos = ftell(f);
   fseek (f, 0, SEEK_END);
   size_t size = ftell (f);
-  rewind (f);
+  fseek (f, pos, SEEK_SET);
   return size;
 }
