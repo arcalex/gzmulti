ba1c61558f6305b3efb8e685c9bfa35f089f3df9
Mahmoud Mubarak <mahmoudmubarak@gmail.com>
2016-11-13 13:59:50 +0200

    main - call gzunpack

--- gzmulti/gzmulti.c
+++ gzmulti/gzmulti.c
@@ -1,7 +1,16 @@
 #include <gzmulti/gzmulti.h>
+#include <string.h>
+#include <stdlib.h>
 
 int
 main (int argc, char **argv)
 {
-  return 0;
+  if (strcmp (argv[1], "unpack") == 0 && argc == 4)
+    {
+      return gzunpack (argv[2], argv[3]);
+    }
+  else
+    {
+      return EXIT_FAILURE;
+    }
 }
